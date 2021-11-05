4b73ce42a6e18df9048144c7a8290d7268862a3e
Dweb Fan <dwebfan@gmail.com>
2021-11-04 21:00:15 -0700

    set port 8000 if no such input
    this is mainly for synology app store
    
    Signed-off-by: Dweb Fan <dwebfan@gmail.com>

--- entry.sh
+++ entry.sh
@@ -3,12 +3,12 @@ set -xe
 
 if [ "$#" -lt 1 ]
 then
-    echo "ERROR: port required"
-    echo "`basename $0` [port]"
-    exit 1
+    echo "listen at 8000"
+    LOMOD_PORT=8000
+else
+    LOMOD_PORT=$1
 fi
 
 #/usr/bin/update-lomod.sh
 
-LOMOD_PORT=$1
 /opt/lomorage/bin/lomod -p $LOMOD_PORT -b /lomo --max-upload 1 --max-fetch-preview 3
